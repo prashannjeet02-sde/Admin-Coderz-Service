86e5e974b3793473c3c73620b055589194c6355d
prashannjeetHexan <prasannjeet.p@hexantech.com>
2026-02-05 11:39:41 +0530

    Initial git setup

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const { PORT } = require("./config/server.config");
+
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.text());
+
+// Server
+app.listen(PORT, () => {
+  console.log(`Server listening to PORT:${PORT}`);
+});
